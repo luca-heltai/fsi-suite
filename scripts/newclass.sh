d637ef066346200a01393d399caa6e3564b7dbcd
Luca Heltai <luca.heltai@sissa.it>
2022-01-10 08:31:33 +0000

    New class script.

---
+++ scripts/newclass.sh
@@ -0,0 +1,9 @@
+cp ../source/template.nocc ../source/$(basename $1).cc
+cp ../include/template.noh ../include/$1.h
+ESCAPED_H=$(printf '%s\n' "$1.h" | sed -e 's/[\/&]/\\&/g')
+sed -i "s/XXX/$ESCAPED_H/"   ../source/$(basename $1).cc
+def=${1/\//\_}
+sed -i "s/XXX/$def/"   ../include/$1.h
+cmake .
+~/.vscode-server/bin/899d46d82c4c95423fb7e10e68eba52050e30ba3/bin/code ../source/$(basename $1).cc
+~/.vscode-server/bin/899d46d82c4c95423fb7e10e68eba52050e30ba3/bin/code ../include/$1.h
