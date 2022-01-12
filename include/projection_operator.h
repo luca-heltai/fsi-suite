10f9fb95dfb3c4c7da1ae707773f3704775a2eaa
Luca Heltai <luca.heltai@sissa.it>
2022-01-12 10:08:10 +0000

    Added projection operator.

---
+++ include/projection_operator.h
@@ -0,0 +1,114 @@
+#ifndef projection_operator_basis_h
+#define projection_operator_basis_h
+
+#include <deal.II/base/config.h>
+
+#include <deal.II/lac/linear_operator_tools.h>
+#include <deal.II/lac/sparse_matrix.h>
+#include <deal.II/lac/vector.h>
+namespace dealii
+{
+  /**
+   * Construct a LinearOperator object that projects a vector onto a basis.
+   *
+   * This function can be used to perform model order reduction, i.e. to project
+   * a large dimensional vector onto a smaller dimensional subspace. Typical
+   * usage of this class is the following:
+   *
+   * @code
+   * // Build a reduced vector space, i.e., a VectorType (either serial or
+   * // parallel) that is an element of the Range space of the LinearOperator
+   * VectorType exemplar_range(...);
+   *
+   * // Initialize a vector of references to the locally owned basis vectors
+   * // onto which we want to project.
+   * std::vector<std::reference_wrapper<VectorType>> basis(...);
+   *
+   * P = projection_operator(exemplar_range, basis);
+   * Pt = transpose_operator(P);
+   *
+   * // Project a vector onto the reduced space
+   * VectorType elements = P * v;
+   *
+   * // If the basis was orthogonal, then we could compute an orthogonal
+   * // projection of v onto the subspace identified by the basis as
+   * VectorType  v_reduced = Pt * v;
+   *
+   * // The above could also be achieved with the following:
+   * VectorType  v_reduced = (Pt*P) * v;
+   *
+   * // If we want to project an operator A onto the reduced space, we could do
+   * // the following:
+   * auto A_reduced = Pt*A*P;
+   * @endcode
+   *
+   * @param range_exemplar An exmeplar vector of the range space. We'll make a
+   * few copies of this vector to allow the operator to be used with
+   * intermediate storage.
+   * @param local_basis A vector of references to the locally owned basis
+   * vectors. These vectors must be ordered according to the
+   * Range::locally_owned_elements() of the range space.
+   * @param domain_exemplar If used in parallel, it may happen that some
+   * processes do not own any index of the Range space. In this case, you need
+   * to provide a pointer to a Domain vector to be used as an exemplar vector.
+   *
+   * @return LinearOperator<Range, Domain, Payload>
+   */
+  template <
+    typename Range,
+    typename Domain,
+    typename Payload = internal::LinearOperatorImplementation::EmptyPayload>
+  LinearOperator<Range, Domain, Payload>
+  projection_operator(
+    const Range &                                            range_exemplar,
+    const std::vector<std::reference_wrapper<const Domain>> &local_basis,
+    const Domain * domain_exemplar = nullptr,
+    const Payload &payload         = Payload())
+  {
+    LinearOperator<Range, Domain, Payload> linear_operator(payload);
+    linear_operator.vmult = [range_exemplar, local_basis](Range &       dst,
+                                                          const Domain &src) {
+      static const auto id = range_exemplar.locally_owned_elements();
+      AssertDimension(local_basis.size(), id.n_elements());
+      unsigned int i = 0;
+      for (const auto j : id)
+        dst[j] = local_basis[i++].get() * src;
+    };
+
+
+    linear_operator.Tvmult = [range_exemplar, local_basis](Domain &     dst,
+                                                           const Range &src) {
+      static const auto id = range_exemplar.locally_owned_elements();
+      AssertDimension(local_basis.size(), id.n_elements());
+      dst            = 0;
+      unsigned int i = 0;
+      for (const auto j : id)
+        dst.sadd(1.0, src[j], local_basis[i++]);
+      dst.compress(VectorOperation::add);
+    };
+
+    linear_operator.reinit_range_vector = [range_exemplar](Range &dst,
+                                                           bool   fast) {
+      dst.reinit(range_exemplar, fast);
+    };
+
+    if (domain_exemplar != nullptr)
+      linear_operator.reinit_domain_vector = [domain_exemplar](Domain &dst,
+                                                               bool    fast) {
+        dst.reinit(*domain_exemplar, fast);
+      };
+    else
+      {
+        Assert(local_basis.size() > 0,
+               ExcMessage("If domain_exemplar is not provided, "
+                          "local_basis must contain at least one element."));
+        linear_operator.reinit_domain_vector = [local_basis](Domain &dst,
+                                                             bool    fast) {
+          dst.reinit(local_basis[0].get(), fast);
+        };
+      }
+    return linear_operator;
+  }
+
+} // namespace dealii
+#endif
---
+++ tests/projection_operator.cc
@@ -0,0 +1,72 @@
+#include <deal.II/base/config.h>
+
+#include "projection_operator.h"
+
+#include <deal.II/lac/vector.h>
+
+#include <gtest/gtest.h>
+
+#include <fstream>
+#include <sstream>
+
+#include "tests.h"
+
+using namespace dealii;
+
+TEST(ProjectionOperator, ProjectionOnSubspace)
+{
+  initlog();
+  // Orthogonal projection on the first M basis
+  const unsigned int          N = 5;
+  const unsigned int          M = 3;
+  std::vector<Vector<double>> v(M, Vector<double>(N));
+  std::vector<std::reference_wrapper<const Vector<double>>> basis(v.begin(),
+                                                                  v.end());
+  for (unsigned int i = 0; i < M; ++i)
+    {
+      v[i][i]  = 1;
+      basis[i] = std::cref(v[i]);
+      deallog << "Basis " << i << ": " << v[i] << std::endl;
+    }
+
+  Vector<double> w(M);
+
+  auto C  = projection_operator(w, basis);
+  auto Ct = transpose_operator(C);
+
+  auto Iw = C * Ct;
+  auto Pw = Ct * C;
+
+  // Try it out: w in R^M
+  for (unsigned int i = 0; i < M; ++i)
+    w[i] = 2 * i + 1;
+  Vector<double> x(N);
+
+  // x in R^N
+  for (unsigned int i = 0; i < N; ++i)
+    x[i] = 2 * i + 1;
+
+  deallog << "w: " << w << std::endl;
+  deallog << "x: " << x << std::endl;
+  double w_norm = w.l2_norm();
+  double x_norm = x.l2_norm();
+
+  Vector<double> y = Ct * w; // in R^N
+  deallog << "Ct*w =  " << y << std::endl;
+
+  Vector<double> p = C * x; // in R^M
+  deallog << "C*x =   " << p << std::endl;
+  ASSERT_DOUBLE_EQ(p.l2_norm(), w_norm); // Same as w
+
+  Vector<double> z = Iw * w; // in R^M
+  deallog << "(C*Ct)*w =  " << z << std::endl;
+  ASSERT_DOUBLE_EQ(z.l2_norm(), w_norm); // Same norm as w
+  z -= w;
+  // They should be actually equal
+  ASSERT_DOUBLE_EQ(z.l2_norm(), 0.0); // Same as w
+
+  Vector<double> q = Pw * x; // in R^N
+  deallog << "(Ct*C)*x = " << q << std::endl;
+  // again, q should have the same norm as w
+  ASSERT_DOUBLE_EQ(p.l2_norm(), w_norm);
+}
