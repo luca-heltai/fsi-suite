8f76296c9498abb335eaae8cb335d7c457a492d5
Luca Heltai <luca.heltai@sissa.it>
2022-02-17 08:54:29 +0000

    Add Hyper_L test.

--- tests/grid_generator.cc
+++ tests/grid_generator.cc
@@ -77,3 +77,31 @@ TYPED_TEST(DimSpacedimTester, GenerateHyperCubeSimplex)
   const unsigned int dims[] = {0, 1, 8, 24};
   ASSERT_EQ(tria.n_active_cells(), dims[this->dim]);
 }
+
+
+
+TYPED_TEST(DimTesterNoOne, GenerateHyperL)
+{
+  ParsedTools::GridGenerator<this->dim, this->spacedim> pgg("/");
+  Triangulation<this->dim, this->spacedim>              tria;
+
+  this->parse(R"(
+    set Input name = hyper_L
+    set Arguments = 0: 1: false
+    set Transform to simplex grid = false
+  )");
+
+  std::string grid_name = "grid_" + std::to_string(this->dim) +
+                          std::to_string(this->spacedim) + ".msh";
+
+  this->parse("set Output name = " + grid_name);
+
+
+  // After this, we should have a file grid.msh
+  pgg.generate(tria);
+  ASSERT_TRUE(std::ifstream(grid_name));
+  std::remove(grid_name.c_str());
+
+  // And the grid should have 3 elements
+  ASSERT_EQ(tria.n_active_cells(), 3u);
+}
